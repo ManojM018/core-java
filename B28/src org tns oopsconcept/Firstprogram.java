616a3c7c9fcd39cc33cef21e14eb4bc83070cf6e
ManojM018 <115932006+ManojM018@users.noreply.github.com>
2022-10-16 19:12:43 +0530

    Create Firstprogram.java

---
+++ B28/src org tns oopsconcept/Firstprogram.java
@@ -0,0 +1,27 @@
+
+package org.tns.oopsconcept;
+public class Firstprogram {
+	int year_of_purchase;
+	String make;
+	int model;
+	double cost;
+	public void display1()
+	{
+		System.out.println("year_of_purchase"+2001);
+		System.out.println("make"+"Suzuki");
+	}
+	void disp2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("cost"+500000);
+	}
+
+	public static void main(String[] args) {
+		
+         Firstprogram ob1=new Firstprogram();
+         ob1.display1();
+         ob1.disp2();
+	}
+
+
+}
